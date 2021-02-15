c9f4b27ef8e84aaefd18b11645e696cf975cd182
Sean Tarzy <seantarzy@gmail.com>
2021-02-14 21:16:25 -0500

    seeded heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
+config.assets.initialize_on_precompile = false
+
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
