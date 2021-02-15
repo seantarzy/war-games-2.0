d62a417213d32b6f5991a2cb6362aa2d53981167
Sean Tarzy <seantarzy@gmail.com>
2021-02-14 20:13:11 -0500

    welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :players
 
-    root 'application#index'
+    root 'welcome#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
